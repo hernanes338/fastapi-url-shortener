ffcad9846c7bd840dfe9c6b249e2711e4ecfb66b
hernanes338 <auherlpe@gmail.com>
2023-06-22 19:21:33 +0200

    get full shortened URL

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -19,3 +19,11 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
+
+# checks the database for an active database entry with the provided secret_key
+def get_db_url_by_admin_key(db: Session, admin_key: str) -> models.URL:  # returns either None or a database entry with a provided key
+    return (
+        db.query(models.URL)
+        .filter(models.URL.admin_url == admin_key)
+        .first()
+    )
--- shortener_app/main.py
+++ shortener_app/main.py
@@ -6,6 +6,10 @@ from sqlalchemy.orm import Session
 from . import models, schemas, crud
 from .database import SessionLocal, engine
 
+from starlette.datastructures import URL
+
+from .config import get_settings
+
 app = FastAPI()
 # binds the database engine
 models.Base.metadata.create_all(bind=engine)  # If the database defined in engine doesn’t exist yet, then it’ll be created with all modeled tables
@@ -24,20 +28,29 @@ def raise_not_found(request):
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API :)"
 
 @app.post("/url", response_model=schemas.URLInfo)
-def create_url(url: schemas.URLBase, db: Session = Depends(get_db)): # requires a URLBase schema as an argument and depends on the database session
+def create_url(
+        url: schemas.URLBase,          
+        db: Session = Depends(get_db)
+    ): # requires a URLBase schema as an argument and depends on the database session
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
     db_url = crud.create_db_url(db=db, url=url) # get the db_url database object back
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 @app.get("/{url_key}")
 def forward_to_target_url(
@@ -47,5 +60,16 @@ def forward_to_target_url(
     ):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key): # walrus operator to assign variables in the middle of an expression
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo)
+def get_url_info(
+        secret_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
